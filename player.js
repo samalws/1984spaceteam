abc63ea36c18e32253cae576991b9e560e7d07ae
Sam Uwe Alws <sam.alws@gmail.com>
2020-04-18 23:08:15 -0400

    Update player.js

--- player.js
+++ player.js
@@ -1,6 +1,13 @@
+// so that all player ids are unique
 var highestPlayerId = 0
-const makePlayer = name => ({id: ++highestPlayerId /* jaden moment */, name: name, loyaltyLost: 0}) // TODO gonna also have stuff like IP and stuff
+// generate a player object with a given name
+// TODO gonna have to do loyalty and stuff
+const makePlayer = name => ({id: ++highestPlayerId /* jaden moment */, name: name, loyaltyLost: 0})
+// penalize a player who did an UNGOOD action (ie an action that no one received a command to do), returns new player object
 const penalizePlayer = player => Object.assign({}, player, {loyaltyLost: player.loyaltyLost + 1})
+// (getting disappeared = kicked from the game)
+// when you do an ungood action (ie when penalizePlayer is called) [loyaltyLostToDisappear] times you get kicked
 const loyaltyLostToDisappear = 10
+// should this player be disappeared?
 const shouldDisappearPlayer = player => player.loyaltyLost > loyaltyLostToDisappear
 const shouldntDisappearPlayer = player => !shouldDisappearPlayer(player)
