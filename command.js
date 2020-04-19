ed9c91f218b6dfafe62610d5711b9d43680c5312
Sam Uwe Alws <sam.alws@gmail.com>
2020-04-18 22:12:35 -0400

    Update command.js

--- command.js
+++ command.js
@@ -1,3 +1,4 @@
 // command is like the things that come in the pipes and tell you what to do
-makeCommand = text => ministryAdds => ({text: text, ministryAdds: ministryAdds}) // jaden moment
-addCommandScore = cmd => ministries => addMinistryScores(cmd.ministryAdds)(ministries)
+makeCommand = text => name => ministryAdds => ({text: text, name: name, ministryAdds: ministryAdds}) // jaden moment
+addCommandScore = command => ministries => addMinistryScores(command.ministryAdds)(ministries)
+commandMatchesAction = command => action => command.name == action.name
