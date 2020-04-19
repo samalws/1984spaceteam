aa266743c69554c07602750fc7d5316566838bc9
Sam Uwe Alws <sam.alws@gmail.com>
2020-04-18 20:22:08 -0400

    Update command.js

--- command.js
+++ command.js
@@ -1,3 +1,3 @@
 // command is like the things that come in the pipes and tell you what to do
 makeCommand = text => ministryAdds => ({text: text, ministryAdds: ministryAdds}) // this is very unnecessary lmao
-runCommand = cmd => ministryScores => addMinistryScores(cmd.ministryAdds)(ministryScores)
+runCommand = cmd => ministries => addMinistryScores(cmd.ministryAdds)(ministries)
