3b4f1f621549ca08cd909ef6702c2139bc78b555
Sam Uwe Alws <sam.alws@gmail.com>
2020-04-18 23:34:49 -0400

    Update action.js

--- action.js
+++ action.js
@@ -1,2 +1,9 @@
-const makeAction = name => funcOnScreen => ({name: name, funcOnScreen: funcOnScreen}) // jaden moment
-const applyActionToScreen = action => action.funcOnScreen // the wonders of currying
+// action = dragging a paper into a pipe
+
+// enum of all the pipes
+const pipes = Object.freeze({miniluv: 0, minipax: 1, miniplenty: 2, minitrue: 3, memoryHole = 4})
+// make an action lol
+// jaden moment
+const makeAction = paperId => pipe => ({paperId: paperId, pipe: pipe})
+// apply the action to the player's screen
+const applyActionToScreen = action => deletePaperFromScreen(action.paperId)
