51d585626b2f8d99e38f8fe636b10ed6c6a4e9d2
Sam Uwe Alws <sam.alws@gmail.com>
2020-04-18 22:59:25 -0400

    Update ministry.js

--- ministry.js
+++ ministry.js
@@ -1,9 +1,20 @@
-const ministry = Object.freeze({miniluv: 0, minipax: 1, miniplenty: 2, minitrue: 3}) // Object.freeze makes an enum apparently
+// enum of all the ministries
+const ministry = Object.freeze({miniluv: 0, minipax: 1, miniplenty: 2, minitrue: 3})
+// generate a ministryScores thing with all the scores set to 0
 const makeMinistryScores = () => ({[ministry.miniluv]: 0, [ministry.minipax]: 0, [ministry.miniplenty]: 0, [ministry.minitrue]: 0})
-const setMinistryScore = ministryToSet => newScore => ministryScores => Object.assign({}, ministryScores, {[ministryToSet]: newScore}) // update a score without changing the original one
-const addMinistryScore = ministryToAdd => toAdd => ministryScores => setMinistryScore(ministryToAdd)(scores[ministryToAdd]+toAdd)(ministryScores) // add a score without changing the original one
+// sets the score of a ministry, returning the new ministryScores
+const setMinistryScore = ministryToSet => newScore => ministryScores => Object.assign({}, ministryScores, {[ministryToSet]: newScore})
+// add to the score of a ministry, returning the new ministryScores
+const addMinistryScore = ministryToAdd => toAdd => ministryScores => setMinistryScore(ministryToAdd)(scores[ministryToAdd]+toAdd)(ministryScores)
+// adds to the scores of multiple ministries, returning the new ministryScores
+// format of adds: {ministry.miniluv: 65, ministry.minipax: 6}
+// addMinistryScores({ministry.miniluv: 65, ministry.minipax: 6})(scores) is equivalent to:
+//   addMinistryScore(ministry.miniluv)(65)(addMinistryScore(ministry.minipax)(6)(scores))
 const addMinistryScores = adds => ministryScores => Object.keys(adds).reduce((scores, key) => addMinistryScore(key)(adds[key])(scores), ministryScores) // add multiple scores in the format of like {miniplenty: 3, miniluv: 2}
 const ministryUngoodLvl = 5
 const ministryDoubleplusUngoodLvl = 10
-const getUngoodMinistries = ministryToCheck => Object.keys(ministryToCheck).filter(x => ministryToCheck[x] < -ministryUngoodLvl || ministryToCheck[x] > ministryUngoodLvl)
-const getDoubleplusUngoodMinistries = ministryToCheck => Object.keys(ministryToCheck).filter(x => ministryToCheck[x] < -ministryDoubleplusUngoodLvl || ministryToCheck[x] > ministryDoubleplusUngoodLvl)
+// return a list of ministries whose scores' absolute values are > ministryUngoodLvl
+// ie list of ministries that're ungood
+const getUngoodMinistries = ministryScores => Object.keys(ministryScores).filter(x => ministryScores[x] < -ministryUngoodLvl || ministryScores[x] > ministryUngoodLvl)
+// ditto but for doubleplusungood
+const getDoubleplusUngoodMinistries = ministryScores => Object.keys(ministryScores).filter(x => ministryScores[x] < -ministryDoubleplusUngoodLvl || ministryScores[x] > ministryDoubleplusUngoodLvl)
