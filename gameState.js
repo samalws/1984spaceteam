83796b0f3af3a0ccb453607502e1612461f8c232
Sam Uwe Alws <sam.alws@gmail.com>
2020-04-18 22:26:37 -0400

    Update gameState.js

--- gameState.js
+++ gameState.js
@@ -4,12 +4,12 @@ const commandDoneOnState = player => gameState => giveCommandOnState(player)(Obj
 const penalizePlayerOnState = player => gameState => Object.assign({}, gameState, {players: Object.assign({}, gameState.players, {[player.id]: penalizePlayer(player)})})
 const applyActionOnState = player => action => gameState => Object.assign({}, gameState, {playerScreens: Object.assign({}, gameState.playerScreens, {[player.id]: applyActionToScreen(action)(playerScreens[player.id])})})
 const playerActionOnState = player => action => gameState => {
-  playerCommanded = gameState.players[playerCommandedForAction(playerCommands)(action)]
+  let playerCommanded = gameState.players[playerCommandedForAction(playerCommands)(action)]
   return applyActionOnState(action)(playerCommanded // apply the action regardless of if it was commanded
     ? commandDoneOnState(playerCommanded)(gameState) // if someone was commanded, then replace their command and update scores
     : penalizePlayerOnState(player)(gameState)) // otherwise, penalize the thoughtcrimer who did something without being told
 }
-const checkForGameLossState = gameState => (getDoubleplusUngood(gameState.ministries).length < 2) && gameState // jaden moment
+const checkForGameLossState = gameState => (getDoubleplusUngoodMinistries(gameState.ministries).length < 2) && gameState // jaden moment
 const tickMinistriesState = gameState => Object.assign({}, gameState, {ministries: tickMinistries(gameState.ministries)})
 const possiblyKickPlayersState = gameState => Object.assign({}, gameState, {players: gameState.players.filter(shouldntKickPlayer)})
 const tick = gameState => checkForGameLossState(tickMinistriesState(possiblyKickPlayersState(contentTick(gameState)))
