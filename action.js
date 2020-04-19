86d9dc97dbe58c2e584836e1db05e09573eb6829
Sam Uwe Alws <sam.alws@gmail.com>
2020-04-19 00:25:53 -0400

    Update action.js

--- action.js
+++ action.js
@@ -2,8 +2,8 @@
 
 // make an action lol
 // jaden moment
-const makeAction = paperId => pipe => ({paperId: paperId, pipe: pipe})
+const makeAction = paper => pipe => ({paper: paper, pipe: pipe})
 // apply the action to the player's screen
 const applyActionToScreen = action => deletePaperFromScreen(action.paperId)
 // check if 2 actions are the same
-const actionsEqual = a => b => a.paperId == b.paperId && a.pipe == b.pipe
+const actionsEqual = a => b => papersEqual(a.paper, b.paper) && a.pipe == b.pipe
