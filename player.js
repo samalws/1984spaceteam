3bb36111f89f58e18a7ebfe9060a116e8d8dfd45
Sam Uwe Alws <sam.alws@gmail.com>
2020-04-18 22:30:15 -0400

    Update player.js

--- player.js
+++ player.js
@@ -1,3 +1,6 @@
 var highestPlayerId = 0
-makePlayer = name => ({id: ++highestPlayerId /* jaden moment */, name: name, loyaltyLost: 0}) // TODO gonna also have stuff like IP and stuff
-penalizePlayer = player => Object.assign({}, player, {loyaltyLost: player.loyaltyLost + 1})
+const makePlayer = name => ({id: ++highestPlayerId /* jaden moment */, name: name, loyaltyLost: 0}) // TODO gonna also have stuff like IP and stuff
+const penalizePlayer = player => Object.assign({}, player, {loyaltyLost: player.loyaltyLost + 1})
+const loyaltyLostToDisappear = 10
+const shouldDisappearPlayer = player => player.loyaltyLost > loyaltyLostToDisappear
+const shouldntDisappearPlayer = player => !shouldDisappearPlayer(player)
