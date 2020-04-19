7be7915deed4d5f7ab3a268474a20cc1d901c6e3
Sam Uwe Alws <sam.alws@gmail.com>
2020-04-18 23:37:50 -0400

    Update command.js

--- command.js
+++ command.js
@@ -6,8 +6,8 @@ makeCommand = text => name => timeLimit => ministryAdds => ({text: text, name: n
 // apply the ministryAdds to the ministry scores
 // CURRYING <3
 addCommandScore = command => addMinistryScores(command.ministryAdds)
-// whether the action is the right thing to do for this command and isn't expired
-commandMatchesAction = command => action => time => command.name == action.name && time < command.timeLimit
+// whether the action is the right thing to do for this command and the command isn't expired
+commandMatchesAction = command => action => time => actionsEqual(command.action, action) && time < command.timeLimit
 // find the player who had the command for an action to be done, returning undefined if that player doesn't exist
 // note that (empty array)[0] == undefined
 playerCommandedForAction = playerCommands => action => Object.keys(playerCommands).filter(key => commandMatchesAction(playerCommands[key], action))[0]
