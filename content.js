cd6d3d4dbf406e6115fba6e214d9f66d224a1054
Sam Uwe Alws <sam.alws@gmail.com>
2020-04-19 00:47:36 -0400

    Create content.js

---
+++ content.js
@@ -0,0 +1,8 @@
+// the game content (random generation etc)
+
+// TODO
+
+makePlayerCommands = players => ()
+makePlayerScreens = players => ()
+genNewCommandForPlayer = player => gameState => ()
+contentTick = tickTime => gameState => gameState
