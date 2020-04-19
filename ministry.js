1bd39a9ebd4a4a450945968763da253d772df332
Sam Uwe Alws <sam.alws@gmail.com>
2020-04-18 20:23:17 -0400

    Update ministry.js

--- ministry.js
+++ ministry.js
@@ -1,7 +1,7 @@
 const ministry = Object.freeze({miniluv: 0, minipax: 1, miniplenty: 2, minitrue: 3}) // Object.freeze makes an enum apparently
 const makeMinistryScores = () => ({[ministry.miniluv]: 0, [ministry.minipax]: 0, [ministry.miniplenty]: 0, [ministry.minitrue]: 0})
-const setMinistryScore = ministryToSet => newScore => scores => Object.assign({}, scores, {[ministryToSet]: newScore}) // update a score without changing the original one
-const addMinistryScore = ministryToAdd => toAdd => scores => setMinistryScore(ministryToAdd)(scores[ministryToAdd]+toAdd)(scores) // add a score without changing the original one
-const addMinistryScores = adds => scores => Object.keys(adds).reduce((scores2, key) => addMinistryScore(key)(adds[key])(scores), scores) // add multiple scores in the format of like {miniplenty: 3, miniluv: 2}
+const setMinistryScore = ministryToSet => newScore => ministryScores => Object.assign({}, ministryScores, {[ministryToSet]: newScore}) // update a score without changing the original one
+const addMinistryScore = ministryToAdd => toAdd => ministryScores => setMinistryScore(ministryToAdd)(scores[ministryToAdd]+toAdd)(ministryScores) // add a score without changing the original one
+const addMinistryScores = adds => ministryScores => Object.keys(adds).reduce((scores, key) => addMinistryScore(key)(adds[key])(scores), ministryScores) // add multiple scores in the format of like {miniplenty: 3, miniluv: 2}
 const ministryDangerLvl = 5
 const getDangerous = ministryToCheck => Object.keys(ministryToCheck).filter(x => ministryToCheck[x] < -ministryDangerLvl || ministryToCheck[x] > ministryDangerLvl) // get which ones are DANGEROUS
