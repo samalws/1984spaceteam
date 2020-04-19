3a1460c3e8165567a7bda9752c0bf5ff71015907
Sam Uwe Alws <sam.alws@gmail.com>
2020-04-18 23:39:55 -0400

    Create pipes.js

---
+++ pipes.js
@@ -0,0 +1,5 @@
+// you have 5 pipes on your screen: 1 for each ministry, and 1 for memory hole
+// also a pipe where things come in but that doesn't count because it's not an output pipe lol
+
+// enum of all the pipes
+const pipes = Object.assign({}, ministries, {memoryHole: 4})
