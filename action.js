061113f5b3943b5a6b4a49c089b7d7ff149cdca5
Sam Uwe Alws <sam.alws@gmail.com>
2020-04-18 23:40:19 -0400

    Update action.js

--- action.js
+++ action.js
@@ -1,7 +1,5 @@
 // action = dragging a paper into a pipe
 
-// enum of all the pipes
-const pipes = Object.freeze({miniluv: 0, minipax: 1, miniplenty: 2, minitrue: 3, memoryHole = 4})
 // make an action lol
 // jaden moment
 const makeAction = paperId => pipe => ({paperId: paperId, pipe: pipe})
