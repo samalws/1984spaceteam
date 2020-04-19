7986268240a81379781cfff168cc6014948a7063
Sam Uwe Alws <sam.alws@gmail.com>
2020-04-18 23:20:39 -0400

    Update command.js

--- command.js
+++ command.js
@@ -1,5 +1,13 @@
 // command is like the things that come in the pipes and tell you what to do
-makeCommand = text => name => ministryAdds => ({text: text, name: name, ministryAdds: ministryAdds}) // jaden moment
-addCommandScore = command => ministries => addMinistryScores(command.ministryAdds)(ministries)
-commandMatchesAction = command => action => command.name == action.name
-playerCommandedForAction = playerCommands => action => Object.keys(playerCommands).filter(key => commandMatchesAction(playerCommands[key], action))[0] // if it's empty it returns undefined
+
+// make a command given THOSE parameters
+// ministryAdds is like the type of argument you give to addMinistryScore
+makeCommand = text => name => timeLimit => ministryAdds => ({text: text, name: name, timeLimit: timeLimit, ministryAdds: ministryAdds}) // jaden moment
+// apply the ministryAdds to the ministry scores
+// CURRYING <3
+addCommandScore = command => addMinistryScores(command.ministryAdds)
+// whether the action is the right thing to do for this command and isn't expired
+commandMatchesAction = command => action => time => command.name == action.name && time < command.timeLimit
+// find the player who had the command for an action to be done, returning undefined if that player doesn't exist
+// note that (empty array)[0] == undefined
+playerCommandedForAction = playerCommands => action => Object.keys(playerCommands).filter(key => commandMatchesAction(playerCommands[key], action))[0]
