b815c90360ba685545b063f0a0185770f56102da
Sam Uwe Alws <sam.alws@gmail.com>
2020-04-18 21:47:07 -0400

    Create gameState.js

---
+++ gameState.js
@@ -0,0 +1,11 @@
+const makeGameState = players => ({players: players, ministryScores: makeMinistryScores(), playerCommands: makePlayerCommands(), playerScreens: makePlayerScreens()})
+const giveCommandOnState = player => gameState => Object.assign({}, gameState, {playerCommands: genNewCommandForPlayer(player)(playerCommands)}) // give a player a new command
+const commandDoneOnState = player => gameState => giveCommandOnState(player)(Object.assign({}, gameState, {ministryScores: addCommandScore(gameState.playerCommands[player.id])(gameState.ministryScores)}))
+const penalizePlayerOnState = player => gameState => Object.assign({}, gameState, {players: Object.assign({}, gameState.players, {[player.id]: penalizePlayer(player)})})
+const applyActionOnState = player => action => gameState => Object.assign({}, gameState, {playerScreens: Object.assign({}, gameState.playerScreens, {[player.id]: applyActionToScreen(action)(playerScreens[player.id])})})
+const playerActionOnState = player => action => gameState => {
+  playerCommanded = playerCommandedForAction(playerCommands)(action)
+  return applyActionOnState(action)(playerCommanded // apply the action regardless of if it was commanded
+    ? commandDoneOnState(playerCommanded)(gameState) // if someone was commanded, then replace their command and update scores
+    : penalizePlayerOnState(player)(gameState)) // otherwise, penalize the thoughtcrimer who did something without being told
+}
