163332f9aa89e315fc1524b90fb2d8ed25853195
Sam Uwe Alws <sam.alws@gmail.com>
2020-04-19 00:39:52 -0400

    Update screen.js

--- screen.js
+++ screen.js
@@ -2,7 +2,7 @@
 
 // jaden moment
 makeScreen = () => []
-// make screens for all the players
+// make paperless screens for all the players
 makePlayerScreens = players => Object.keys(players).reduce((acc, id) => Object.assign(acc, {[id]: makeScreen()}), {})
 // add a paper to a screen, returning new screen
 addPaperToScreen = paper => screen => [...screen, paper]
